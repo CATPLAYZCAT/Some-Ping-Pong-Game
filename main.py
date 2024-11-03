aef817089ae22e7442b6f9a6b8a3a462e1864ba9
CATPLAYZCAT <zhaoyik12@gmail.com>
2024-11-03 17:08:22 +0800

    Updated V1.0.1

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ window = display.set_mode((700,500))
 window_width = 700
 window_height = 500
 
-speed_x = 3
-speed_y = 3
+
+
+speed_x = 5
+speed_y = 5
 
 isGamerunning = True
+isFinish = False
+isCollided = False
 clock = time.Clock()
 FPS = 60
 
@@ -35,41 +39,60 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 420:
+        if keys[K_DOWN] and self.rect.y < 280:
             self.rect.y += self.speed
 
     def update_L(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 420:
+        if keys[K_s] and self.rect.y < 280:
             self.rect.y += self.speed
 
 
 class Enemy(GameSprite):
    def update(self):
        self.rect.x += self.speed
-       global miss
-       if self.rect.x > window_height:
-           self.rect.y  = randint(0, window_height - 80)
-           self.rect.x = 0
 
-Left_Player = Player("rocket.png", 0,50,15,80,300)
-Right_Player = Player("rocket.png", 620,50,15,80,300)
-Ball = Enemy("ufo.png",0,0,0,125,100)
+Left_Player = Player("rocket.png", 0,50,10,60,220)
+Right_Player = Player("rocket.png", 620,50,10,60,220)
+Ball = Enemy("ufo.png",350,250,0,100,75)
 
 
 while isGamerunning:
     window.blit(transform.scale(image.load("galaxy.jpg"), (700,500)), (0,0))
     Left_Player.reset()
     Right_Player.reset()
-    Ball.reset()
     Left_Player.update_L()
     Right_Player.update_R()
-    Ball.update()
+  
 
-    #if sprite.collide_rect(Left_Player, Ball) or sprite.collide_rect(Right_Player,Ball):
+    if isFinish != True:
+        Ball.reset()
+        Ball.update()
+        Ball.rect.x += speed_x
+        Ball.rect.y += speed_y
 
+    if sprite.collide_rect(Left_Player, Ball) or sprite.collide_rect(Right_Player,Ball):
+        rng = randint(1,2)
+        if isCollided == False:
+            isCollided = True
+            speed_x *= -1
+            speed_y *= -1
+            isCollided = False
+
+    if Ball.rect.y > 400 or Ball.rect.y < 0:
+        speed_y *= -1
+        
+    if Ball.rect.x < 0:
+        isFinish = True
+        isGamerunning = False
+        print("P1 LOSE!")
+    
+    if Ball.rect.x > 700:
+        isFinish = True
+        isGamerunning = False
+        print("P2 LOSE!")
 
     display.update()
     clock.tick(FPS)
